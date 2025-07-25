af76e4c9a1b09138abedb8d76aa7f8e04b1c5849
Bhavesh Addala <116192735+bhavesh1502@users.noreply.github.com>
2025-07-25 14:15:09 +0530

    added a message in code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask"
+    return "Hello from Flask 2nd time"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
