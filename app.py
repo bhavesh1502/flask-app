b74abee1146d05d97b539f7d07d3241dd9b538b0
Bhavesh Addala <116192735+bhavesh1502@users.noreply.github.com>
2025-07-28 17:27:17 +0530

    edited buildspec

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask-App"
+    return "This is my 1st ECS deployment"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
